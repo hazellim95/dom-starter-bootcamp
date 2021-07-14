298a6ce52f49795dae37fcb17ee3710d47d09dd2
Hazel Lim <Hazel@Admins-MacBook-Pro-3.local>
2021-07-14 22:06:34 +0800

    element contents from an array of arrays

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Please implement exercise logic here
-
-// Global const for word array
-const words = ['banana', 'pasta', 'alligator', 'purple', 'accurate', 'fickle'];
+const words = [
+  ['orange', 'tomato'],
+  ['fire engine', 'basketball'],
+];
 
 for (i = 0; i < 6; i++) {
   //Create grey container to hold row
@@ -9,33 +10,81 @@ for (i = 0; i < 6; i++) {
   container.innerText = '';
   container.classList.add('container');
   console.log(`created grey box ${i}`);
-  
-  // Create 1 yellow row1
-  const row = document.createElement('div')
-  row.innerText = '';
-  row.classList.add('row');
 
-  //Create local array to store the 6 word elements
-  const wordArray = [];
-  //Loop through words array to create 6 pink boxes
-  for (k = 0; k < words.length; k++) {
-    const word = document.createElement('span');
-    word.innerText = words[k];
-    word.classList.add('word');
-    console.log(`created word ${k}: ${words[k]}`)
-    wordArray.push(word);
+  // Create 2 yellow rows using for loop and push to a new array
+  const rowElements = [];
+  for (j = 0; j < words.length; j++) {
+    const row = document.createElement('div')
+    row.innerText = '';
+    row.classList.add('row');
+    rowElements.push(row);
   }
+
   //Append container to document
   document.body.appendChild(container);
   console.log(`appending grey box ${i}`);
 
-  //Append row to container
-  container.appendChild(row);
-  //Append words to row
-  for (j = 0; j < wordArray.length; j++) {
-    row.appendChild(wordArray[j]);
+  
+  console.log("creating words..");
+  //For loop through each row
+  for (k = 0; k < rowElements.length; k++) {
+    // create temporary variable for this row element
+    const tempRow = rowElements[k];
+    // append it to the container
+    container.appendChild(tempRow);
+
+    // create 2 word elements and append each element to the respective row element
+    const wordArray = words[k];
+    for (m = 0; m < wordArray.length; m++) {
+      const word = document.createElement('span');
+      word.innerText = wordArray[m];
+      word.classList.add('word');
+      // append to this row
+      tempRow.appendChild(word);
+    }
   }
+
 }
+
+
+
+//// Previous exercise: Element Contents From an Array: ////
+// // Global const for word array
+// const words = ['banana', 'pasta', 'alligator', 'purple', 'accurate', 'fickle'];
+
+// for (i = 0; i < 6; i++) {
+//   //Create grey container to hold row
+//   const container = document.createElement('div');
+//   container.innerText = '';
+//   container.classList.add('container');
+//   console.log(`created grey box ${i}`);
+  
+//   // Create 1 yellow row
+//   const row = document.createElement('div')
+//   row.innerText = '';
+//   row.classList.add('row');
+
+//   //Create local array to store the 6 word elements
+//   const wordArray = [];
+//   //Loop through words array to create 6 pink boxes
+//   for (k = 0; k < words.length; k++) {
+//     const word = document.createElement('span');
+//     word.innerText = words[k];
+//     word.classList.add('word');
+//     console.log(`created word ${k}: ${words[k]}`)
+//     wordArray.push(word);
+//   }
+//   //Append container to document
+//   document.body.appendChild(container);
+//   console.log(`appending grey box ${i}`);
+
+//   //Append row to container
+//   container.appendChild(row);
+//   //Append words to row
+//   for (j = 0; j < wordArray.length; j++) {
+//     row.appendChild(wordArray[j]);
+//   }
+// }
 
 
 
