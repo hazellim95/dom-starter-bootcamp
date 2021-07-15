f340bb559c1fb785719e5104d85006d05afa2de5
Hazel Lim <Hazel@Admins-MacBook-Pro-3.local>
2021-07-15 10:55:52 +0800

    events - click and input

--- events/script.js
+++ events/script.js
@@ -5,49 +5,53 @@ const words = [
 
 //global variable to store current grey container element
 let container = document.createElement('div');
+//global variable to store current yellow box element
+let row = document.createElement('div');
 
-// Function that creates yellow boxes, with parameter of type array
+// Function that gets the text from input element and returns it as a string
+const getInput = () => {
+  //Get the value from input field
+  const text = document.getElementById('input-text').value;
+  //const text = document.getElementsByClassName('input').value;
+  console.log('input value');
+  console.log(text);
+  return text;
+}
 
-const createYellowBox = (array) => {
+// Function that creates pink boxes with words and returns the pink box element
+const createPinkBox = (string) => {
+  const word = document.createElement('span');
+  word.innerText = string;
+  word.classList.add('word');
+  return word;
+}
+
+// Function that creates yellow boxes
+const createYellowBox = () => {
   // Create 1 yellow row 
-  const row = document.createElement('div')
+  row = document.createElement('div')
   row.innerText = '';
   row.classList.add('row');
 
-  // get the elements from the input array and create an element for each 
-  console.log("creating word elements..");
-  for (m = 0; m < array.length; m++) {
-    const word = document.createElement('span');
-    word.innerText = array[m];
-    word.classList.add('word');
-    //append each element to the respective row element    
-    row.appendChild(word);
-  }
-  // return the created yellow box with its words
-  return row;
-}
+  //get input from this input field
+  const input = getInput()
 
-// function to create second yellow box and append it to the container parameter
-const createSecondYellowBox = () => {
-  // Create the yellow row
-  const row = createYellowBox(words[0]);
-  //append it to container
+  //Create the word
+  const word = createPinkBox(input);
+
+  //append word to row
+  row.append(word);
+  
+  //append row to the container
   container.appendChild(row);
 }
-
-// // Function that adds button to container
-// function addButtonToContainer() {
-//     const container = document.getElementById('buttonContainer');
-//     const button = document.createElement('button');
-//     button.innerText = 'submit';
-//     container.appendChild(button);
-// }
 
 
 // Function that creates grey box
 const createContainer = (theEvent) => {
   console.log('Creating box');
   //Create grey container to hold row - assign it to global 
+  //const container = document.createElement('div');
   container = document.createElement('div');
   container.innerText = '';
   container.classList.add('container');
@@ -56,23 +60,32 @@ const createContainer = (theEvent) => {
   document.body.appendChild(container);
   console.log(`appending grey box`);
 
-  // Create the yellow rows using for loop and push to a new array
-  const rowElements = []; //new array to store yellow rows
-  for (j = 0; j < words.length; j++) {
-    const row = createYellowBox(words[j])
-    rowElements.push(row);
-  }
-  //Append rows to container
-  for (k = 0; k < rowElements.length; k++) {
-    container.appendChild(rowElements[k]);
-  }
+  // // Create the yellow rows using for loop and push to a new array
+  // const rowElements = []; //new array to store yellow rows
+  // for (j = 0; j < words.length; j++) {
+  //   const row = createYellowBox(words[j])
+  //   rowElements.push(row);
+  // }
+  // //Append rows to container
+  // for (k = 0; k < rowElements.length; k++) {
+  //   container.appendChild(rowElements[k]);
+  // }
 
-  //Create internal button for creating another row
+  //create input field
+  const input = document.createElement('input');
+  input.innerText = '';
+  input.classList.add('input');
+  input.setAttribute('id', 'input-text')
+  //append input field to container
+  container.appendChild(input);
+
+  //create internal button
   const button2 = document.createElement('button');
   button2.innerText = 'submit';
   button2.classList.add('button');
+
   //Add eventlistener to button2 - if this is clicked, create another yellow box and append it above the submit button
-  button2.addEventListener('click', createSecondYellowBox);
+  button2.addEventListener('click', createYellowBox);
   //Append button2 to container
   container.appendChild(button2);
 }
